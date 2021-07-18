9c7ec54cbc6b59b9e6934327d63a680de4230478
Samuel <fasanyasam@yahooo.com>
2021-07-18 22:27:16 +0100

    sign up form done

---
+++ script.js
@@ -0,0 +1,9 @@
+var body = document.getElementsByName("body")
+
+function alertMessage(){
+    var popupMessage = alert("Student Account Successfully Created!")
+    submit= document.getElementById("submit");
+    submit.onclick = popupMessage;
+
+
+}
